0ec48cf40d82bb7e265451276a8da8c6e3be435c
Felipe Chernicharo <string7dev@gmail.com>
2021-03-02 20:27:11 -0300

    refForwardingComponent -> passa ref pro filho

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,17 +1,20 @@
-import React, { InputHTMLAttributes } from 'react';
+import React, { forwardRef, InputHTMLAttributes } from 'react';
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   name: string;
   id: string;
 }
 
-const Input: React.FC<InputProps> = ({ name, id, ...rest }: InputProps) => {
+const Input: React.RefForwardingComponent<HTMLInputElement, InputProps> = (
+  { name, id, ...rest }: InputProps,
+  ref
+) => {
   return (
     <label htmlFor={id}>
       <span>{name}</span>
-      <input type="text" id={id} name={name} {...rest} />
+      <input type="text" id={id} name={name} {...rest} ref={ref} />
     </label>
   );
 };
 
-export default Input;
+export default forwardRef(Input);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,22 @@
-import Head from 'next/head';
-import { SyntheticEvent, useCallback, useMemo, useRef, useState } from 'react';
+import { useCallback, useMemo, useRef, useState } from 'react';
 import Input from '../components/Input';
 import styles from '../styles/Home.module.css';
 
-interface FormData {
-  name: string;
-}
-
 export default function Home() {
-  const submitForm = useCallback((e: React.FormEvent) => {
-    e.preventDefault();
-    console.log('form submitted');
+  const nameInputRef = useRef<HTMLInputElement>(null);
+
+  const handleSubmit = useCallback(e => {
+    e.preventDefault(); // <- recarrega a página se não tiver preventDefault
+    // console.log('form submitted');
+    console.log(nameInputRef.current.value);
   }, []);
 
   return (
     <div>
       <h1>Hello Ref</h1>
 
-      <form onSubmit={e => submitForm(e)}>
-        <Input id="first" name="First Name" />
+      <form onSubmit={e => handleSubmit(e)}>
+        <Input id="first" name="First Name" ref={nameInputRef} />
       </form>
     </div>
   );
