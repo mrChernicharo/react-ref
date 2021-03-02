1a18f75a13cf1b793d920e286827a64ae71d01c1
Felipe Chernicharo <string7dev@gmail.com>
2021-03-02 16:25:19 -0300

    usa ref em inputs

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,52 @@
 import Head from 'next/head';
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import styles from '../styles/Home.module.css';
 
+interface FormData {
+  name: string;
+  email: string;
+  password: string;
+}
+
 export default function Home() {
-  const nameInputRef = useRef(null);
-  const emailInputRef = useRef(null);
-  const passwordInputRef = useRef(null);
+  const [result, setResult] = useState({} as FormData);
+
+  const nameRef = useRef(null);
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
+
+  function submitForm() {
+    const form = {
+      name: nameRef.current.value,
+      email: emailRef.current.value,
+      password: passwordRef.current.value,
+    };
+
+    setResult(form);
+  }
 
   return (
     <div>
       <h1>Hello Ref</h1>
+      <div className="result">{JSON.stringify(result)}</div>
+      <br />
+
       <label htmlFor="user-name">
         User Name
-        <input ref={nameInputRef} id="user-name" type="text" />
+        <input ref={nameRef} id="user-name" type="text" />
       </label>
       <label htmlFor="email">
         Email
-        <input ref={emailInputRef} id="email" type="text" />
+        <input ref={emailRef} id="email" type="text" />
       </label>
       <label htmlFor="password">
         Password
-        <input ref={passwordInputRef} id="password" type="text" />
+        <input ref={passwordRef} id="password" type="text" />
       </label>
+
+      <button type="button" onClick={submitForm}>
+        Submit
+      </button>
     </div>
   );
 }
