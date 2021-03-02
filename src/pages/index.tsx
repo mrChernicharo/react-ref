7177772b55e31a416c1fe8f7a699613ae79d023b
Felipe Chernicharo <string7dev@gmail.com>
2021-03-02 20:32:07 -0300

    submita com keyDown enter

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,11 +4,17 @@ import styles from '../styles/Home.module.css';
 
 export default function Home() {
   const nameInputRef = useRef<HTMLInputElement>(null);
+  const emailInputRef = useRef<HTMLInputElement>(null);
 
   const handleSubmit = useCallback(e => {
     e.preventDefault(); // <- recarrega a página se não tiver preventDefault
     // console.log('form submitted');
     console.log(nameInputRef.current.value);
+    console.log(emailInputRef.current.value);
+  }, []);
+
+  const handleEnterKey = useCallback(e => {
+    if (e.code === 'Enter') handleSubmit(e);
   }, []);
 
   return (
@@ -16,7 +22,8 @@ export default function Home() {
       <h1>Hello Ref</h1>
 
       <form onSubmit={e => handleSubmit(e)}>
-        <Input id="first" name="First Name" ref={nameInputRef} />
+        <Input id="name" name="name" ref={nameInputRef} />
+        <Input id="email" name="email" ref={emailInputRef} onKeyDown={e => handleEnterKey(e)} />
       </form>
     </div>
   );
