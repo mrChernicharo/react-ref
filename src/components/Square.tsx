b8e48daff997441ad256fd007ddd146d17de0522
Felipe Chernicharo <string7dev@gmail.com>
2021-03-02 23:14:12 -0300

    brincando com squares

---
+++ src/components/Square.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import styles from '../styles/Home.module.css';
+interface Props {}
+
+const Square = () => {
+  return <div className={styles.square}>Square</div>;
+};
+
+export default Square;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,30 +1,34 @@
-import { useCallback, useMemo, useRef, useState } from 'react';
+import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import Input from '../components/Input';
+import Square from '../components/Square';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
-  const nameInputRef = useRef<HTMLInputElement>(null);
-  const emailInputRef = useRef<HTMLInputElement>(null);
+  const [squares, setSquares] = useState([1, 2, 3, 4, 5, 6]);
 
-  const handleSubmit = useCallback(e => {
-    e.preventDefault(); // <- recarrega a página se não tiver preventDefault
-    // console.log('form submitted');
-    console.log(nameInputRef.current.value);
-    console.log(emailInputRef.current.value);
-  }, []);
+  const handleIncrement = useCallback(() => {
+    const addedSquares = [...squares, squares.length + 1];
+    setSquares(addedSquares);
+  }, [squares]);
 
-  const handleEnterKey = useCallback(e => {
-    if (e.code === 'Enter') handleSubmit(e);
-  }, []);
+  const handleDecrement = useCallback(() => {
+    const last = squares.length;
+    const copy = [...squares];
+    copy.splice(last - 1, 1);
+
+    setSquares(copy);
+  }, [squares]);
 
   return (
     <div>
-      <h1>Hello Ref</h1>
+      <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+        {squares.map((square, i) => (
+          <Square key={i} />
+        ))}
+      </div>
 
-      <form onSubmit={e => handleSubmit(e)}>
-        <Input id="name" name="name" ref={nameInputRef} />
-        <Input id="email" name="email" ref={emailInputRef} onKeyDown={e => handleEnterKey(e)} />
-      </form>
+      <button onClick={handleIncrement}>add</button>
+      <button onClick={handleDecrement}>remove</button>
     </div>
   );
 }
